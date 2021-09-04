6bc79db70993ffee8ee200a449e35019430f7819
yjl <2514515093@qq.com>
2021-09-04 22:58:38 +0800

    test

---
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.service.impl;
+
+import com.example.entity.User;
+import com.example.mapper.UserMapper;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author:0xOO
+ * @Date: 2018/9/26 0026
+ * @Time: 15:23
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper userMapper;
+    public User Sel(int id){
+        return userMapper.Sel(id);
+    }
+}
